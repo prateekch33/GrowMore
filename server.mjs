cc18fb71f2dae4d6da5bd36cde5aa8d9d98f6abd
prateekch33 <prateekchandra27@outlook.com>
2023-05-11 02:43:33 +0530

    Vercel Added and Server Updated

--- server.mjs
+++ server.mjs
@@ -6,6 +6,7 @@ import connect from "./backend/db.mjs";
 import { errorHandler, notFound } from "./backend/middlewares/errorhandler.mjs";
 import auth from "./backend/routes/auth.mjs";
 import tasksAdd from "./backend/routes/tasksAdd.mjs";
+import path from "path";
 dotenv.config();
 const app = express();
 app.use(cors());
@@ -14,12 +15,24 @@ app.use(bodyParser.json());
 const port = process.env.PORT;
 connect();
 
-app.get("/", (req, res) => {
-  res.send("API Working Successfully!!");
-});
+// app.get("/", (req, res) => {
+//   res.send("API Working Successfully!!");
+// });
+app.use("/api/auth", auth);
+app.use("/api/task", tasksAdd);
 
-app.use("/auth", auth);
-app.use("/task", tasksAdd);
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static(path.resolve("./", "frontend", "build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve("./", "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
+
 app.use(notFound);
 app.use(errorHandler);
 
